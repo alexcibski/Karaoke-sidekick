ee19593e8eaa593908d530b26ae682ae6eb273fe
alcib-22 <ciborowski.alex@gmail.com>
2023-09-15 20:15:34 -0700

    style changes on homepage

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,36 +1,55 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
-import { Container, Segment, Header, Grid } from 'semantic-ui-react';
+import { Container, Segment, Header, Grid, Icon } from 'semantic-ui-react';
+
 
 export default function HomePage() {
     return (
-        <Container centered="true">
+        <Container className="home-container">
             <Grid centered>
                 <Grid.Column mobile={16} tablet={10} computer={8}>
-                    <Segment style={{ background: 'lavender', color: 'white' }}>
-                        <Header as="h1">Home</Header>
-                        <p style={{color: 'purple'}}>Welcome to Karaoke Sidekick!! Click a link below to get started!!</p>
+                    <Segment className="home-segment">
+                        <Header as="h1" className="home-header">
+                            Welcome to Karaoke Sidekick!
+                        </Header>
+                        <p className="home-description">
+                            Click a link below to get started!!
+                        </p>
                         <Link to="/pop">
-                            <Segment as="div" textAlign='center'>Sing a pop classic</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="music" size="big" />
+                                Sing a pop classic
+                            </Segment>
                         </Link>
-                        <br />
                         <Link to="/easy">
-                            <Segment as="div" textAlign='center'>Sing something easy</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="star" size="big" />
+                                Sing something easy
+                            </Segment>
                         </Link>
-                        <br />
                         <Link to="/gifted">
-                            <Segment as="div" textAlign='center'>For the gifted singer</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="microphone" size="big" />
+                                For the gifted singer
+                            </Segment>
                         </Link>
-                        <br />
                         <Link to="/pina">
-                            <Segment as="div" textAlign='center'>If you like Pina Coladas...</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="cocktail" size="big" />
+                                If you like Pina Coladas...
+                            </Segment>
                         </Link>
-                        <br />
                         <Link to="/hype">
-                            <Segment as="div" textAlign='center'>Get everyone hyped up!!!</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="group" size="big" />
+                                Get everyone hyped up!!!
+                            </Segment>
                         </Link>
                         <Link to="/slow">
-                        <Segment as="div" textAlign='center'>Slow things wayy down</Segment>
+                            <Segment className="home-link-segment" textAlign='center'>
+                                <Icon name="moon" size="big" />
+                                Slow things way down
+                            </Segment>
                         </Link>
                     </Segment>
                 </Grid.Column>
@@ -38,3 +57,9 @@ export default function HomePage() {
         </Container>
     );
 }
+
+
+
+
+
+
